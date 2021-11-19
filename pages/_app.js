e8372a2a55386debe2ea49388afede90551d18e7
Ansh Narula <2019210@iiitdmj.ac.in>
2021-11-19 20:51:15 +0530

    public images

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,37 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta
+          name="description"
+          content="Know about me, see my works, and connect with me!"
+        ></meta>
+        <meta
+          name="description"
+          content="Know about me, see my works, and connect with me!"
+        ></meta>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crossOrigin="anonymous"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+          rel="stylesheet"
+        />
+        <title>Ansh Narula</title>
+        {/* <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+          crossOrigin="anonymous"
+        ></script> */}
       </Head>
-      <Component {...pageProps}  />
-      
-        
+      <Component {...pageProps} />
     </>
   );
 }

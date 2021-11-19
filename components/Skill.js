b3a04f794504620acc52963c208ff0f19479e3ac
Ansh Narula <2019210@iiitdmj.ac.in>
2021-11-19 15:42:04 +0530

    bug fixed

--- components/Skill.js
+++ components/Skill.js
@@ -7,13 +7,13 @@ import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 
 const Skill = ({name, img, width, mt = 2, text = "texta", delay = 50, cName = ""}) => {
-  // const Sk = styled.div`
-  //   transform: translateZ(30px);
-  //   width: ${width};
-  //   @media (max-width: 768px) {
-  //   width: 3rem;
-  // }
-  // `
+  const Sk = styled.div`
+    transform: translateZ(30px);
+    width: ${width};
+    @media (max-width: 768px) {
+    width: 3rem;
+  }
+  `
   const options = {
     scale: 1.2,
     speed: 600,
@@ -26,9 +26,9 @@ const Skill = ({name, img, width, mt = 2, text = "texta", delay = 50, cName = ""
     <span className="col-md-2 col-3 mt-3 d-flex flex-column align-items-center d-inline stack hide ">
       <span className={text}>{name}</span>
       <Tilt style={{ transformStyle: "preserve-3d" }} options={options}>
-          <div className = "skill mt-1">
+          <Sk className = "skill mt-1">
           <Image src={img}  alt="" layout="responsive" className ={cName}/>
-          </div>
+          </Sk>
 
       </Tilt>
     </span>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
-import About from "./#about";
-import Connect from "./#connect";
-import Projects from "./#projects";
-import Tech from "./#tech";
+import About from "./About";
+import Connect from "./connect";
+import Projects from "./projects";
+import Tech from "./tech";
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
